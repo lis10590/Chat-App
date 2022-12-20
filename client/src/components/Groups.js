df5a0d4ff824ea54ccd729edd78eb7e408223d86
lis10590 <lisset10590@gmail.com>
2022-12-20 17:04:46 +0200

    added groups and chat

--- client/src/components/Groups.js
+++ client/src/components/Groups.js
@@ -1,12 +1,28 @@
 import { Panel, Button } from "react-bulma-companion";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { getAllGroups, selectAllGroups } from "../store/groups";
 
 const Groups = () => {
+  const dispatch = useDispatch();
+  const groups = useSelector(selectAllGroups);
+
+  useEffect(() => {
+    dispatch(getAllGroups());
+  }, [dispatch]);
+
   return (
     <div>
       <Button>+</Button>
-      <Panel></Panel>
+      <Panel>
+        {groups.map((group) => {
+          return (
+            <Panel.Block>
+              <img style={{ height: "5rem" }} src={group.profile_pic} />
+            </Panel.Block>
+          );
+        })}
+      </Panel>
     </div>
   );
 };
