d68762e0322723e501c537d399ccbad219f9cbb9
Lis Setgasy <lisset10590@gmail.com>
2023-01-15 21:18:41 +0200

    added origin to cors

--- server/main.py
+++ server/main.py
@@ -25,7 +25,7 @@ class JSONEncoder(json.JSONEncoder):
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 jwt = JWTManager(app)
-socketio = SocketIO(app, "*")
+socketio = SocketIO(app, cors_allowed_origins="*")
 CORS(app,origins=os.environ.get("ORIGIN"))
 
 
