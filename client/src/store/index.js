1ca47f1dd7d3ebd14beeb9aec28ea662f58825b1
lis10590 <lisset10590@gmail.com>
2023-01-15 16:06:55 +0200

    deleted registered

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,6 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
 import usersReducer from "./users";
-import regUsersReducer from "./registered";
 import groupsReducer from "./groups";
 import authReducer from "./auth";
 import messagesReducer from "./messages";
@@ -9,7 +8,6 @@ import blockedReducer from "./blocked";
 const store = configureStore({
   reducer: {
     users: usersReducer,
-    registered: regUsersReducer,
     groups: groupsReducer,
     auth: authReducer,
     messages: messagesReducer,
